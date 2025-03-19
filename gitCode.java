b8f48a57a73437ab70ae734d15e8b92bb9ab6cb7
Gourav Patra <gouravpatra7478@gmail.com>
2025-03-19 15:53:00 +0530

    sknka

--- gitCode.java
+++ gitCode.java
@@ -1,6 +1,7 @@
 public class gitCode {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int n = 10;
         System.out.println("Hello, World!");
         System.out.println("Good Morning");
     }
